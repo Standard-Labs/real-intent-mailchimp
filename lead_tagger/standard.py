f57b2c3de89e65723e2ab261af411c17902f3438
Ayush <ayushsh285@gmail.com>
2025-04-29 10:39:29 -0400

    use set to ensure uniqueness

--- lead_tagger/standard.py
+++ lead_tagger/standard.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 class StandardTagger(BaseTagger):
     def generate_tags(self, row: pd.Series) -> str:
-        tags = []
+        tags: set = set()
 
         for col, tags_list in self.tag_mapping.items():
             if pd.notna(row.get(col)):
-                tags.extend(tags_list)
+                tags.update(tags_list)
 
-        return ', '.join(tags)
+        return ', '.join(sorted(tags))
